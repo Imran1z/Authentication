24a4e5e28bab2d12adb1fbe64797193d36d392e2
Imran1z <shaikhzimran12334@gmail.com>
2024-02-08 10:46:09 +0530

    created server using mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 
-app.listen(3000,()=>{
-    console.log('Server is up and running on port:',3000)
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('Connected to Mongodb')
+    app.listen(process.env.PORT,()=>{
+        console.log('Server is up and running on port:',process.env.PORT)
+    })
+}).catch((err)=>{
+    console.log(err)
 })
+
+
