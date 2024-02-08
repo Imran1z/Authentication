b4b0bfe1f2e070c9a30d0e01dbea1292c5716828
Imran1z <shaikhzimran12334@gmail.com>
2024-02-08 18:21:43 +0530

    create sign in api route with token cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 
 
@@ -16,4 +17,32 @@ export const signup=async(req, res,next)=>{
      next(error)
     // next(errorHandler(300,"something went wrong"))
  }
+}
+
+
+export const signin =async(req, res, next)=>{
+   const {email,password}=req.body;
+
+   try {
+      const validUser =await User.findOne({email});
+      if (!validUser) {
+         return next(errorHandler(401,'User not found'));
+      }
+
+      const validPassword =bcryptjs.compareSync(password,validUser.password);
+      if (!validPassword) {
+         return next(errorHandler(401, "Wrong Email or Password"));
+      }
+
+      const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET,{expiresIn:'24h'});
+
+      const {password:hashedPassword,...user}=validUser._doc;
+
+      const expiryDate = new Date(Date.now()+(24 * 60 * 60 * 1000));
+
+      res.cookie('token',token ,{httpOnly:true,expires:expiryDate}).status(200).json({user});
+
+   } catch (error) {
+      next(error)
+   }
 }
