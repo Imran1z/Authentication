f1001d5fce524b25155f60210aff85d4ff6d6a9e
Imran1z <shaikhzimran12334@gmail.com>
2024-02-10 09:39:33 +0530

    firebase image upload method

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,16 +1,98 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-
+import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage'
+import {app} from '../firebase.js'
 
 const Profile = () => {
-  const {currentUser}=useSelector((state)=>state.user)
-  console.log(currentUser)
+  const {currentUser}=useSelector((state)=>state.user);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0)
+  const [imageError, setImageError] = useState(false)
+  const [FormData, setFormData] = useState({})
+  console.log(FormData)
+  console.log(imagePercent)
+  useEffect(()=>{
+    if (image) {
+      handleFileUpload(image)
+      
+    }
+  },[image]);
+
+
+
+  
+
+  // const handleFileUpload=async(image)=>{
+  //   const storage=getStorage(app);
+  //   const fileName = new Date().getTime()+image.name;
+  //   const storageRef = ref(storage, fileName);
+  //   const uploadTask= uploadBytesResumable(storageRef,image);
+  //   uploadTask.on(
+  //     'state_changed',
+  //     (snapshot)=>{
+  //       const progress =(snapshot.bytesTransferred/snapshot.totalBytes) *100;
+  //       //console.log('Upload is '+progress+"% done");
+  //       setImagePercentage(Math.round(progress))
+
+  //     },
+   
+  //   (error)=>{
+  //     setImageError(true)
+  //   },
+  //   ()=>{
+  //     getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>{
+  //       setFormData({...FormData,profile:downloadUrl})
+
+  //     })
+  //   }
+  //   );
+
+  //   ///4:00:37
+  // }
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...FormData, profile: downloadURL })
+        );
+      }
+    );
+  };
+
+  const fileRef =useRef();
+
+  const handleImageClick = () => {
+    fileRef.current.click(); // Trigger the file input dialog
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form className='flex flex-col gap-4' >
 
-            <img src={`${currentUser.user.profile}`} alt="Profile" className='h-24 w-24 self-center cursor-pointer rounded-full mb-5 object-cover hover:h-[100px]  hover:w-[100px]'/>
+            <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e)=>setImage(e.target.files[0])}/>
+
+                                  {/* allow read;
+                            allow write: if 
+                            request.resource.size<2*1024*1024 &&
+                            request.resource.contentType.matches('images/.*') */}
+
+            <img src={`${currentUser.user.profile}`} alt="Profile" className='h-24 w-24 self-center cursor-pointer rounded-full mb-5 object-cover hover:h-[100px]  hover:w-[100px]'
+              onClick={handleImageClick}
+            />
 
             <input type="username" defaultValue={currentUser.user.username} placeholder='Username' id='password' className='bg-slate-100 p-3 rounded-lg'/> 
             
